3882d4373579b0480b887d48a0911724aef42883
Genzinc24 <globalizethunder@gmail.com>
2026-06-29 09:36:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const events = [
   { date: "June 6", title: "Enrollment", type: "Enrollment" },
   { date: "June 17", title: "Start of Classes", type: "Enrollment" },
+  { date: "June 7- July 15", title: "BEIS Updating of School Profile", type: "Deadlines" },
 ];
 
 const calendarDiv = document.getElementById("calendarEvents");
